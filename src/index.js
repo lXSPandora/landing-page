958412eeade7e827bcf4bef27a035835ce14d5e5
lXSPandora <luizepauloxd@gmail.com>
2018-08-08 15:11:06 -0300

    ✨ Adds Apollo Client and React Router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/index.css';
-import App from './App';
+import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { client } from './config/apolloClient';
+import Home from './screens/Home/Home';
+import Articles from './screens/Articles/Articles';
 import registerServiceWorker from './worker/registerServiceWorker';
+import './css/index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/articles" component={Articles} />
+        </div>
+    </BrowserRouter>
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
