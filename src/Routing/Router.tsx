de051d86e94d2510b0ef9d2696895d6cd52acf3f
Luiz Fernando Sousa Camargo <luizepauloxd@gmail.com>
2018-10-25 06:30:38 -0300

    ✨ Adds new routes to router

--- src/Routing/Router.tsx
+++ src/Routing/Router.tsx
@@ -2,9 +2,15 @@ import * as React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Landing from '../screens/Landing/Landing';
+import Articles from '../screens/Articles/Articles';
+import Contact from '../screens/Contact/Contact';
 
 export default () => (
   <BrowserRouter>
-    <Route exact={true} component={Landing} />
+    <div>
+      <Route exact={true} path="/" component={Landing} />
+      <Route path="/articles" component={Articles} />
+      <Route path="/contact" component={Contact} />
+    </div>
   </BrowserRouter>
 )
